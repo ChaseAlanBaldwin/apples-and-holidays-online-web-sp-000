06f5ad710390844b6c28e2ff72c40d0eb9b973fd
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-12-12 02:46:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |seasons, holidays|
-    puts "#{seasons}:"
+    puts "#{seasons.capitalize} :"
     holidays.each do |occasion, supplies|
       puts "#{occasion}: #{supplies.join(", ")}"
     end
